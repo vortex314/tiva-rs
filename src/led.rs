fb05e2c4ee685428332765f0f4f32fcf769569a3
Lieven Merckx <lieven.merckx@ing.com>
2023-10-19 19:01:14 +0200

    searching for a model

--- src/led.rs
+++ src/led.rs
@@ -1,66 +1,108 @@
-use alloc::sync::Arc;
-use core::borrow::BorrowMut;
-use core::cell::RefCell;
 use core::convert::Infallible;
-use cortex_m::interrupt::free;
-use cortex_m::interrupt::Mutex;
 use embedded_hal::digital::v1::OutputPin;
-
-use core::pin::pin;
-use futures::select_biased;
-use futures::FutureExt;
-use lilos;
-use lilos::exec::PeriodicGate;
 use thingbuf as conn;
-use tm4c123x_hal as hal;
-use tm4c123x_hal::gpio::Output;
-use tm4c123x_hal::gpio::PushPull;
+use alloc::vec::Vec;
 
+trait TimeClient {
+    fn on_timer(&mut self,timer_id: u32);
+}
+enum TimeServerMsg {
+    WakeMsg,
+    Interval(u32),
+    Gate(u32),
+}
+
+struct TimerServer<'a> {
+    clients : Vec<&'a mut dyn TimeClient>,
+}
+
+impl<'a> TimerServer<'a> {
+    fn new_interval(&mut self, interval: u32,client:&'a mut dyn TimeClient) {
+        self.clients.push(client);
+    }
+
+    fn new_gate(&mut self, interval: u32,client:&'a mut dyn TimeClient) {
+        self.clients.push(client);
+    }
+
+    fn one_shot(&mut self, interval: u32,client:&'a mut dyn TimeClient) {
+        self.clients.push(client);
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum LedMsg {
+    On,
+    Off,
+    Interval(u32),
+    BlinkTimer,
+}
+
+impl Default for LedMsg {
+    fn default() -> Self {
+        LedMsg::On
+    }
+}
 
 pub struct Led<'a> {
-    pin: &'a mut  dyn OutputPin,
-    gate: PeriodicGate,
-    recv_cmd: conn::mpsc::Receiver<bool>,
-    send_cmd: conn::mpsc::Sender<bool>,
+    pin: &'a mut dyn OutputPin,
+    led_state: bool,
+    recv_main: conn::mpsc::Receiver<LedMsg>,
+    send_main: conn::mpsc::Sender<LedMsg>,
     active: bool,
 }
+
+impl TimeClient for Led<'_> {
+    fn on_timer(&mut self, timer_id: u32) {
+        let _ = self.send_main.send(LedMsg::BlinkTimer);
+    }
+}
+
 impl<'a> Led<'a> {
-    pub fn new(pin: &'a mut dyn OutputPin ) -> Self {
-        let (send_cmd, recv_cmd) = conn::mpsc::channel::<bool>(10);
-        let gate = PeriodicGate::from(lilos::time::Millis(500));
+    pub fn new(pin: &'a mut dyn OutputPin) -> Self {
+        let (send_main, recv_main) = conn::mpsc::channel::<LedMsg>(10);
         Self {
             pin,
-            gate,
-            send_cmd,
-            recv_cmd,
+            led_state: false,
+            send_main,
+            recv_main,
             active: true,
         }
     }
 
-    async fn cmd(&mut self, v: bool) {
-        let _ = self.send_cmd.send(v).await;
+    pub fn on(&mut self, msg: LedMsg) {
+        let _ = self.send_main.send(msg);
     }
 
-    async fn blink(&mut self) -> Infallible {
-        loop {
-            self.gate.next_time().await;
-            let _ = self.pin.set_high();
-            self.gate.next_time().await;
-            let _ = self.pin.set_low();
-        }
-    }
-    async fn on_cmd(&mut self) -> Infallible {
-        loop {
-            let cmd = self.recv_cmd.recv().await;
-            if let Some(x) = cmd {
-                self.active = x;
+    fn toggle(&mut self ){
+        if self.active {
+            if self.led_state {
+                self.pin.set_low();
+                self.led_state = false;
+            } else {
+                self.pin.set_high();
+                self.led_state = true;
             }
         }
     }
+
+    fn set_active(&mut self, active: bool) {
+        self.active = active;
+        self.led_state=false;
+        self.pin.set_low();
+    }
+
     pub async fn run(&mut self) -> Infallible {
-  //      loop {
-        self.blink().await
-  //      self.on_cmd().await;
-  //      }
+        loop {
+            let msg = self.recv_main.recv().await;
+            if let Some(m) = msg {
+                match m {
+                    LedMsg::On => self.set_active(true),
+                    LedMsg::Off => self.set_active(false),
+                    LedMsg::BlinkTimer => self.toggle(),
+                    LedMsg::Interval(ms) => {}
+                }
+            }
+        }
     }
 }
