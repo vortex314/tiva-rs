c62ccfb3b58b0679acb8f91b0599bb6af064d32c
lieven <lieven.merckx@gmail.com>
2023-11-05 03:23:38 +0100

    embryo actors

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 #![no_std]
 #![feature(type_alias_impl_trait)]
+#![feature(async_fn_in_trait)]
 //#![feature(noop_waker)]
 #![no_main]
 #![allow(deprecated)]
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(unused_mut)]
+#![feature(const_type_id)]
 
 use alloc::boxed::Box;
+use alloc::vec::Vec;
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::channel;
+use embassy_sync::channel::Channel;
+use embassy_sync::channel::DynamicSender;
+use embassy_sync::channel::Sender;
 use core::any::Any;
+use core::any::TypeId;
 use core::cell::UnsafeCell;
 use core::convert::Infallible;
 use core::fmt::Write;
+use core::ops::ShrAssign;
 use core::task::Poll;
 use cortex_m::peripheral::SYST;
 use cortex_m_rt::exception;
@@ -62,7 +72,6 @@ extern crate alloc;
 use core::option::Option::Some;
 
 use crate::limero::Sink;
-use thingbuf as conn;
 
 /*
 #[panic_handler]
@@ -96,12 +105,149 @@ use timer_driver::Clock;
 mod serde_actor;
 use serde_actor::SerdeActor;
 
+#[embassy_executor::task]
+async fn timer_server_task() {
+
+    loop {
+        hprintln!("timer_server_task");
+    }
+}
+
+struct Emitter<'a,T> {
+    sender:Option<DynamicSender<'a,T>>,
+}
+
+impl<'a,T> Emitter<'a,T> {
+    pub fn new() -> Self {
+        Emitter{sender:None}
+    }
+    pub fn bind(&mut self, sender : DynamicSender<'a,T>) {
+        if self.sender.is_none() {
+            self.sender = Some(sender);
+        }
+    }
+    pub fn pipe<const SIZE:usize>(&mut self,pipe:&  'a Pipe<T,SIZE>) {
+        self.bind(pipe.sender());
+    }
+    pub async fn emit(&mut self, value:T) {
+        self.sender.as_mut().unwrap().send(value).await; //todo test for some
+    }
+}
+
+struct Receptor<'a,T> {
+    receiver:Option<channel::DynamicReceiver<'a,T>>,
+}
+
+impl<'a,T> Receptor<'a,T> {
+    pub fn new() -> Self {
+        Receptor{receiver:None}
+    }
+    pub fn bind(&mut self, receiver : channel::DynamicReceiver<'a,T>) {
+        if self.receiver.is_none() {
+            self.receiver = Some(receiver);
+        }
+    }
+    pub fn pipe<const SIZE:usize>(&mut self,pipe:&  'a Pipe<T,SIZE>) {
+        self.bind(pipe.receiver());
+    }
+    pub async fn poll(&mut self) -> T {
+        self.receiver.as_mut().unwrap().receive().await
+    }
+}
+
+
+
+
+struct Actor1<'a> {
+    pub emitter:Emitter<'a,u32>,
+}
+
+struct Actor2<'a> {
+    pub receptor:Receptor<'a,u32>,
+}
+
+impl Actor2<'_> {
+    pub async fn run(&mut self) {
+        loop {
+            let x = self.receptor.poll().await;
+            hprintln!("x={}",x);
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn actor1_task(mut actor1: Actor1<'static>) {
+    actor1.emitter.emit(1).await;
+    actor1.emitter.emit(2).await;
+}
+
+#[embassy_executor::task]
+async fn actor2_task(mut actor2: Actor2<'static>) {
+    loop {
+        let x = actor2.receptor.poll().await;
+        hprintln!("x={}",x);
+    }
+}
+
+struct Pipe<T,const SIZE:usize> {
+    channel : Channel<NoopRawMutex, T, SIZE>,
+}
+
+impl<T,const SIZE:usize> Pipe<T,SIZE> {
+    pub fn new( ) -> Self {
+        Pipe{channel:Channel::<NoopRawMutex, T, SIZE>::new()}
+    }
+    pub fn sender(&self) -> DynamicSender<T> {
+        self.channel.sender().into()
+    }
+    pub fn receiver(&self) -> channel::DynamicReceiver<T> {
+        self.channel.receiver().into()
+    }
+}
+
+impl<'a,T,const S:usize> ShrAssign<&'a Pipe<T,S>> for Emitter<'a,T> {
+    fn shr_assign(&mut self, rhs: & 'a Pipe<T,S>) {
+        self.bind(rhs.sender());
+    }
+}
+
+impl<'a,T,const S:usize> ShrAssign<&'a Receptor<'a,T>> for Pipe<T,S> {
+    fn shr_assign(&mut self, rhs: & 'a Receptor<'a,T>) {
+    //    rhs.bind(self.receiver());
+    }
+}
+/* 
+fn queue<'a,T,const SIZE:usize>() -> (DynamicSender<'a,T>, channel::DynamicReceiver<'a,T>) {
+    let channel: Channel<NoopRawMutex, T, SIZE> = Channel::<NoopRawMutex, T, SIZE>::new();
+    (channel.sender().into(),channel.receiver().into())
+}*/
+
+
 // #[cortex_m_rt::entry]
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     heap_setup();
+    hprintln!("main started"   );
+    let mut actor1 = Actor1{emitter:Emitter::new()};
+    let mut actor2 = Actor2{receptor:Receptor::new()};
+    let mut actor3 = Actor2{receptor:Receptor::new()};
+    let channel1 = Channel::<NoopRawMutex, u32, 10>::new();
+    let pipe1 = Pipe::<u32,10>::new();
+    actor1.emitter >>= &pipe1;
+    actor1.emitter.pipe(&pipe1);
+    actor2.receptor.pipe(&pipe1);
+ //   let mut actor1 = Box::pin(actor1);
+ //   let mut actor2 = Box::pin(actor2);
+    actor1.emitter.bind(channel1.sender().into());
+    actor2.receptor.bind(channel1.receiver().into());
+    actor3.receptor.bind(channel1.receiver().into());
+    actor1.emitter.emit(1).await   ;
+    actor1.emitter.emit(2).await   ;
+    actor2.run().await;
 
-    let (tx, rx) = conn::mpsc::channel::<String>(10);
+  //  actor1.emitter >> queue(10) >> actor2.receptor;
+  //  actor1.emitter >> transform(|x| x+1) >> actor2.receptor;
+
     let mut peripherals = hal::Peripherals::take().unwrap();
     let mut sysctl = peripherals.SYSCTL.constrain();
     let mut porta_parts = peripherals.GPIO_PORTA.split(&sysctl.power_control);
@@ -158,9 +304,9 @@ async fn main(spawner: Spawner) {
     });*/
 
     let (mut tx, mut rx) = uart.split();
-    static mut uart_actor:UnsafeCell<Uart> = UnsafeCell::new(Uart::new(&mut rx, &mut tx));
+  //  static mut uart_actor:UnsafeCell<Uart> = UnsafeCell::new(Uart::new(&mut rx, &mut tx));
     let mut serde_actor = SerdeActor::new();
-    &mut serde_actor.txd_source >> uart_actor.txd_sink;
+ //   &mut serde_actor.txd_source >> uart_actor.txd_sink;
     //   &mut uart_actor.rxd_source >> &serde_actor.rxd_sink;
     serde_actor.publish("src/tiva/sys/started", &"started");
     serde_actor.publish("src/tiva/sys/heap", &ALLOCATOR.free());
